8b33961eb5b70d5f7f6394716314463d7426f7bd
Jeff Dickey <jdickey@seven-sigma.com>
2017-07-25 18:09:52 +0800

    Convert AllDroplets::DataRequest to use Chamber
    Also a mild refactoring/cleanup.

--- lib/do_api_scripting/api/all_droplets/data_request.rb
+++ lib/do_api_scripting/api/all_droplets/data_request.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
+require 'chamber'
 require 'excon'
 require 'json'
 
 require_relative './non_stubs'
+
+# This **must** be done before any Chamber environment is accessed. Repeated
+# calls to `Chamber.load` apparently (and logically) are harmless, only adding a
+# performance penalty in the event of large Chamber config files being parsed.
+
+Chamber.load basepath: "#{ENV['PWD']}/exe"
 
 # Code to support scripting the DigitalOcean API, e.g., for use with Ansible.
 module DoApiScripting
@@ -12,14 +19,12 @@ module DoApiScripting
     class AllDroplets
       # Class encapsulating sending request to/receiving response from DO API.
       class DataRequest
-        def self.get(auth_header: :from_env, request_module: NonStubs)
+        def self.get(auth_header: :default, request_module: NonStubs)
           new(auth_header, request_module).get
         end
 
         def get
-          resp = stubs.request(headers: headers, url: URL)
-          body = JSON.parse(resp.body, symbolize_names: true)
-          Struct.new(:status, :body).new resp.status, body
+          Struct.new(:status, :body).new response.status, body
         end
 
         protected
@@ -27,9 +32,8 @@ module DoApiScripting
         def initialize(auth_header_in, stub_module)
           @stubs = stub_module
           @auth_header = auth_header_in
-          if auth_header_in == :from_env
-            @auth_header = "Bearer #{ENV['DO_API_TOKEN']}"
-          end
+          @auth_header = DEFAULT_AUTH_HEADER if auth_header_in == :default
+          @response = nil
           self
         end
 
@@ -37,14 +41,24 @@ module DoApiScripting
 
         attr_reader :auth_header, :stubs
 
+        DEFAULT_AUTH_HEADER = "Bearer #{Chamber.env.do.api_token}"
+
         URL = 'https://api.digitalocean.com/v2/droplets'
         private_constant :URL
+
+        def body
+          JSON.parse(response.body, symbolize_names: true)
+        end
 
         def headers
           {
             'Content-Type': 'application/json',
             'Authorization': auth_header
           }
+        end
+
+        def response
+          @response ||= stubs.request(headers: headers, url: URL)
         end
       end # class DoApiScripting::API::AllDroplets::DataRequest
     end # class DoApiScripting::API::AllDroplets
